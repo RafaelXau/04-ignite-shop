3f4bec881b1241fd20255e2288590550d38bd1ee
Rafael Honório <rafaelxhs@gmail.com>
2023-02-13 22:30:56 -0300

    Header atualizado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from 'next/app'
 import Image from 'next/image';
 import { globalStyles } from '@/styles/global'
-import { Container, Header } from '@/styles/pages/app';
+import { Container, Header, ShoppingCartButton } from '@/styles/pages/app';
 
 import logoImg from '../assets/logo.svg';
+import { Handbag } from 'phosphor-react';
 
 globalStyles();
 
@@ -12,6 +13,11 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
+
+        <ShoppingCartButton>
+          <Handbag weight="bold" size={24} />
+          <span>1</span>
+        </ShoppingCartButton>
       </Header>
 
       <Component {...pageProps} />
